a895543455b81f480786a296cd8aec4c9fcffe8a
NabilaAzzahrra <zahrrranabill11@gmail.com>
2024-07-25 22:18:28 +0700

    gelembung

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,22 @@
 import Image from "next/image";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      dshbcjhsdh
+      <div className="bubble-background">
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        <div className="bubble"></div>
+        {/* Add more bubbles as needed */}
+      </div>
+      <div className="content">dshbcjhsdh</div>
     </main>
   );
 }
